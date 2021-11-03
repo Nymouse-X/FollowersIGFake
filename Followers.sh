711b362de12071509f61e82c020972eb4874fda4
NymouseX <saibaougokasu@gmail.com>
2021-11-03 13:12:59 +0700

    Followers Ig

---
+++ Followers.sh
@@ -0,0 +1,454 @@
+echo "1"
+clear
+echo "2"
+clear
+echo "3"
+clear
+echo "4"
+clear
+echo "5"
+clear
+echo "6"
+clear
+echo "7"
+clear
+echo "8"
+clear
+echo "9"
+clear
+echo "10"
+clear
+echo "11"
+clear
+echo "12"
+clear
+echo "13"
+clear
+echo "14"
+clear
+echo "15"
+clear
+echo "16"
+clear
+echo "17"
+clear
+echo "18"
+clear
+echo "19"
+clear
+echo "20"
+clear
+echo "21"
+clear
+echo "22"
+clear
+echo "23"
+clear
+echo "24"
+clear
+echo "25"
+clear
+echo "26"
+clear
+echo "27"
+clear
+echo "28"
+clear
+echo "29"
+clear
+echo "30"
+clear
+echo "31"
+clear
+echo "32"
+clear
+echo "33"
+clear
+echo "34"
+clear
+echo "35"
+clear
+echo "36"
+clear
+echo "37"
+clear
+echo "38"
+clear
+echo "39"
+clear
+echo "40"
+clear
+echo "41"
+clear
+echo "42"
+clear
+echo "43"
+clear
+echo "44"
+clear
+echo "45"
+clear
+echo "46"
+clear
+echo "47"
+clear
+echo "48"
+clear
+echo "49"
+clear
+echo "50"
+clear
+echo "
+███████╗░█████╗░██╗░░░░░██╗░░░░░░█████╗░░██╗░░░░░░░██╗███████╗██████╗░░██████╗
+██╔════╝██╔══██╗██║░░░░░██║░░░░░██╔══██╗░██║░░██╗░░██║██╔════╝██╔══██╗██╔════╝
+█████╗░░██║░░██║██║░░░░░██║░░░░░██║░░██║░╚██╗████╗██╔╝█████╗░░██████╔╝╚█████╗░
+██╔══╝░░██║░░██║██║░░░░░██║░░░░░██║░░██║░░████╔═████║░██╔══╝░░██╔══██╗░╚═══██╗
+██║░░░░░╚█████╔╝███████╗███████╗╚█████╔╝░░╚██╔╝░╚██╔╝░███████╗██║░░██║██████╔╝
+╚═╝░░░░░░╚════╝░╚══════╝╚══════╝░╚════╝░░░░╚═╝░░░╚═╝░░╚══════╝╚═╝░░╚═╝╚═════╝░
+
+██╗░██████╗░
+██║██╔════╝░
+██║██║░░██╗░
+██║██║░░╚██╗
+██║╚██████╔╝
+╚═╝░╚═════╝░" | lolcat
+echo "***********************NYX*********************************"|lolcat
+echo "Author By   : Nymousex"|lolcat
+echo "My Team     : Cyber Hanter Exploit"|lolcat
+echo "My Youtube  : Nymousex" | lolcat
+echo "***********************NYX*********************************"|lolcat
+sleep 1
+echo "User Name :"
+read -p " |NYX}--> " NM
+echo "Berapa Folowers Kamu Saat ini"
+read -p " |NYX}--> " FLW
+echo "Masukan Link IG kamu"
+read -p " |NYX}--> " LINK
+clear
+sleep 1
+echo "Peroses Perbaikan Termux Sedang Berjalan""[>                              <]"
+sleep 0.1
+clear
+echo "Peroses Perbaikan Termux Sedang Berjalan""[>>                            <<]"
+sleep 0.1
+clear
+echo "Peroses Perbaikan Termux Sedang Berjalan""[>>>                          <<<]"
+sleep 0.1
+clear
+echo "Peroses Perbaikan Termux Sedang Berjalan""[>>>>                        <<<<]"
+sleep 0.1
+clear
+echo "Peroses Perbaikan Termux Sedang Berjalan""[>>>>>                      <<<<<]"
+sleep 0.1
+clear
+echo "Peroses Perbaikan Termux Sedang Berjalan""[>>>>>>                    <<<<<<]"
+sleep 0.1
+clear
+echo "Peroses Perbaikan Termux Sedang Berjalan""[>>>>>>>                  <<<<<<<]"
+sleep 0.1
+clear
+echo "Peroses Perbaikan Termux Sedang Berjalan""[>>>>>>>>                <<<<<<<<]"
+sleep 0.1
+clear
+echo "Peroses Perbaikan Termux Sedang Berjalan""[>>>>>>>>>              <<<<<<<<<]"
+sleep 0.1
+clear
+echo "Peroses Perbaikan Termux Sedang Berjalan""[>>>>>>>>>>            <<<<<<<<<<]"
+sleep 0.1
+clear
+echo "Peroses Perbaikan Termux Sedang Berjalan""[>>>>>>>>>>>          <<<<<<<<<<<]"
+sleep 0.1
+clear
+echo "Peroses Perbaikan Termux Sedang Berjalan""[>>>>>>>>>>>>        <<<<<<<<<<<<]"
+sleep 0.1
+clear
+echo "Peroses Perbaikan Termux Sedang Berjalan""[>>>>>>>>>>>>>      <<<<<<<<<<<<<]"
+sleep 0.1
+clear
+echo "Peroses Perbaikan Termux Sedang Berjalan""[>>>>>>>>>>>>>>    <<<<<<<<<<<<<<]"
+sleep 0.1
+clear
+echo "Peroses Perbaikan Termux Sedang Berjalan""[>>>>>>>>>>>>>>>^^<<<<<<<<<<<<<<<]"
+sleep 0.1
+clear
+echo "Peroses Perbaikan Termux Sedang Berjalan""[>>>>>>>>>>>>>>>^^<<<<<<<<<<<<<<<]"
+sleep 0.1
+clear
+echo "Peroses Perbaikan Termux Sedang Berjalan""[>>>>>>>>>>>>>>    <<<<<<<<<<<<<<]"
+sleep 0.1
+clear
+echo "Peroses Perbaikan Termux Sedang Berjalan""[>>>>>>>>>>>>>      <<<<<<<<<<<<<]"
+sleep 0.1
+clear
+echo "Peroses Perbaikan Termux Sedang Berjalan""[>>>>>>>>>>>>        <<<<<<<<<<<<]"
+sleep 0.1
+clear
+echo "Peroses Perbaikan Termux Sedang Berjalan""[>>>>>>>>>>>          <<<<<<<<<<<]"
+sleep 0.1
+clear
+echo "Peroses Perbaikan Termux Sedang Berjalan""[>>>>>>>>>>            <<<<<<<<<<]"
+sleep 0.1
+clear
+echo "Peroses Perbaikan Termux Sedang Berjalan""[>>>>>>>>>              <<<<<<<<<]"
+sleep 0.1
+clear
+echo "Peroses Perbaikan Termux Sedang Berjalan""[>>>>>>>>                <<<<<<<<]"
+sleep 0.1
+clear
+echo "Peroses Perbaikan Termux Sedang Berjalan""[>>>>>>>                  <<<<<<<]"
+sleep 0.1
+clear
+echo "Peroses Perbaikan Termux Sedang Berjalan""[>>>>>>                    <<<<<<]"
+sleep 0.1
+clear
+echo "Peroses Perbaikan Termux Sedang Berjalan""[>>>>>                      <<<<<]"
+sleep 0.1
+clear
+echo "Peroses Perbaikan Termux Sedang Berjalan""[>>>>                        <<<<]"
+sleep 0.1
+clear
+echo "Peroses Perbaikan Termux Sedang Berjalan""[>>>                          <<<]"
+sleep 0.1
+clear
+echo "Peroses Perbaikan Termux Sedang Berjalan""[>>                            <<]"
+sleep 0.1
+clear
+echo "Peroses Perbaikan Termux Sedang Berjalan""[>                              <]"
+sleep 0.1
+clear
+echo "Peroses Perbaikan Termux Sedang Berjalan""[>                              <]"
+sleep 0.1
+clear
+echo "Peroses Perbaikan Termux Sedang Berjalan""[>>                            <<]"
+sleep 0.1
+clear
+echo "Peroses Perbaikan Termux Sedang Berjalan""[>>>                          <<<]"
+sleep 0.1
+clear
+echo "Peroses Perbaikan Termux Sedang Berjalan""[>>>>                        <<<<]"
+sleep 0.1
+clear
+echo "Peroses Perbaikan Termux Sedang Berjalan""[>>>>>                      <<<<<]"
+sleep 0.1
+clear
+echo "Peroses Perbaikan Termux Sedang Berjalan""[>>>>>>                    <<<<<<]"
+sleep 0.1
+clear
+echo "Peroses Perbaikan Termux Sedang Berjalan""[>>>>>>>                  <<<<<<<]"
+sleep 0.1
+clear
+echo "Peroses Perbaikan Termux Sedang Berjalan""[>>>>>>>>                <<<<<<<<]"
+sleep 0.1
+clear
+echo "Peroses Perbaikan Termux Sedang Berjalan""[>>>>>>>>>              <<<<<<<<<]"
+sleep 0.1
+clear
+echo "Peroses Perbaikan Termux Sedang Berjalan""[>>>>>>>>>>            <<<<<<<<<<]"
+sleep 0.1
+clear
+echo "Peroses Perbaikan Termux Sedang Berjalan""[>>>>>>>>>>>          <<<<<<<<<<<]"
+sleep 0.1
+clear
+echo "Peroses Perbaikan Termux Sedang Berjalan""[>>>>>>>>>>>>        <<<<<<<<<<<<]"
+sleep 0.1
+clear
+echo "Peroses Perbaikan Termux Sedang Berjalan""[>>>>>>>>>>>>>      <<<<<<<<<<<<<]"
+sleep 0.1
+clear
+echo "Peroses Perbaikan Termux Sedang Berjalan""[>>>>>>>>>>>>>>    <<<<<<<<<<<<<<]"
+sleep 0.1
+clear
+echo "Peroses Perbaikan Termux Sedang Berjalan""[>>>>>>>>>>>>>>>^^<<<<<<<<<<<<<<<]"
+sleep 0.1
+clear
+echo "Peroses Perbaikan Termux Sedang Berjalan""[>>>>>>>>>>>>>>>^^<<<<<<<<<<<<<<<]"
+sleep 0.1
+pkg remove -y game-repo
+pkg remove -y science-repo
+pkg update
+clear
+echo "Peroses Perbaikan Termux Sedang Berjalan""[>>>>>>>>>>>>>>>^^<<<<<<<<<<<<<<<]"
+sleep 0.1
+echo "Peroses Perbaikan Termux Sedang Berjalan""[>>>>>>>>>>>>>>    <<<<<<<<<<<<<<]"
+sleep 0.1
+clear
+echo "Peroses Perbaikan Termux Sedang Berjalan""[>>>>>>>>>>>>>      <<<<<<<<<<<<<]"
+sleep 0.1
+clear
+echo "Peroses Perbaikan Termux Sedang Berjalan""[>>>>>>>>>>>>        <<<<<<<<<<<<]"
+sleep 0.1
+clear
+echo "Peroses Perbaikan Termux Sedang Berjalan""[>>>>>>>>>>>          <<<<<<<<<<<]"
+sleep 0.1
+clear
+echo "Peroses Perbaikan Termux Sedang Berjalan""[>>>>>>>>>>            <<<<<<<<<<]"
+sleep 0.1
+clear
+echo "Peroses Perbaikan Termux Sedang Berjalan""[>>>>>>>>>              <<<<<<<<<]"
+sleep 0.1
+clear
+echo "Peroses Perbaikan Termux Sedang Berjalan""[>>>>>>>>                <<<<<<<<]"
+sleep 0.1
+clear
+echo "Peroses Perbaikan Termux Sedang Berjalan""[>>>>>>>                  <<<<<<<]"
+sleep 0.1
+clear
+echo "Peroses Perbaikan Termux Sedang Berjalan""[>>>>>>                    <<<<<<]"
+sleep 0.1
+clear
+echo "Peroses Perbaikan Termux Sedang Berjalan""[>>>>>                      <<<<<]"
+sleep 0.1
+clear
+echo "Peroses Perbaikan Termux Sedang Berjalan""[>>>>                        <<<<]"
+sleep 0.1
+clear
+echo "Peroses Perbaikan Termux Sedang Berjalan""[>>>                          <<<]"
+sleep 0.1
+clear
+echo "Peroses Perbaikan Termux Sedang Berjalan""[>>                            <<]"
+sleep 0.1
+clear
+echo "Peroses Perbaikan Termux Sedang Berjalan""[>                              <]"
+sleep 0.1
+clear
+echo "Peroses Perbaikan Termux Sedang Berjalan""[>                              <]"
+sleep 0.1
+clear
+echo "Peroses Perbaikan Termux Sedang Berjalan""[>>                            <<]"
+sleep 0.1
+clear
+echo "Peroses Perbaikan Termux Sedang Berjalan""[>>>                          <<<]"
+sleep 0.1
+clear
+echo "Peroses Perbaikan Termux Sedang Berjalan""[>>>>                        <<<<]"
+sleep 0.1
+clear
+echo "Peroses Perbaikan Termux Sedang Berjalan""[>>>>>                      <<<<<]"
+sleep 0.1
+clear
+echo "Peroses Perbaikan Termux Sedang Berjalan""[>>>>>>                    <<<<<<]"
+sleep 0.1
+clear
+echo "Peroses Perbaikan Termux Sedang Berjalan""[>>>>>>>                  <<<<<<<]"
+sleep 0.1
+clear
+echo "Peroses Perbaikan Termux Sedang Berjalan""[>>>>>>>>                <<<<<<<<]"
+sleep 0.1
+clear
+echo "Peroses Perbaikan Termux Sedang Berjalan""[>>>>>>>>>              <<<<<<<<<]"
+sleep 0.1
+clear
+echo "Peroses Perbaikan Termux Sedang Berjalan""[>>>>>>>>>>            <<<<<<<<<<]"
+sleep 0.1
+clear
+echo "Peroses Perbaikan Termux Sedang Berjalan""[>>>>>>>>>>>          <<<<<<<<<<<]"
+sleep 0.1
+clear
+echo "Peroses Perbaikan Termux Sedang Berjalan""[>>>>>>>>>>>>        <<<<<<<<<<<<]"
+sleep 0.1
+clear
+echo "Peroses Perbaikan Termux Sedang Berjalan""[>>>>>>>>>>>>>      <<<<<<<<<<<<<]"
+sleep 0.1
+clear
+echo "Peroses Perbaikan Termux Sedang Berjalan""[>>>>>>>>>>>>>>    <<<<<<<<<<<<<<]"
+sleep 0.1
+clear
+echo "***********************NYX*********************************"|lolcat
+echo "User Name : $NM"|lolcat
+echo "Folowers Kamu Saat Ini : $FLW"|lolcat
+echo "Link IG : $LINK" | lolcat
+echo "***********************NYX*********************************"|lolcat
+sleep 1
+echo 
+echo "Selamat Di  Penambah Folowers" | lolcat
+echo 
+echo "Silakan Pilih Nominal:" | lolcat
+echo "MAX (1-50)?"
+read -p "|$NM}-->" JWB
+
+if [ $JWB = 1 ] || [ $JWB = 2 ] || [ $JWB = 3 ] || [ $JWB = 4 ] || [ $JWB = 5 ] || [ $JWB = 6 ] || [ $JWB = 7 ] || [ $JWB = 8 ] || [ $JWB = 9 ] || [ $JWB = 10 ] || [ $JWB = 11 ] || [ $JWB = 12 ] || [ $JWB = 13 ] || [ $JWB = 14 ] || [ $JWB = 15 ] || [ $JWB = 16 ] || [ $JWB = 17 ] || [ $JWB = 18 ] || [ $JWB = 19 ] || [ $JWB = 20 ] || [ $JWB = 21 ] || [ $JWB = 22 ] || [ $JWB = 23 ] || [ $JWB = 24 ] || [ $JWB = 25 ] || [ $JWB = 26 ] || [ $JWB = 27 ] || [ $JWB = 28 ] || [ $JWB = 29 ] || [ $JWB = 30 ] || [ $JWB = 31 ] || [ $JWB = 32 ] || [ $JWB = 33 ] || [ $JWB = 34 ] || [ $JWB = 35 ] || [ $JWB = 36 ] || [ $JWB = 37 ] || [ $JWB = 38 ] || [ $JWB = 39 ] || [ $JWB = 40 ] || [ $JWB = 41 ] || [ $JWB = 42 ] || [ $JWB = 43 ] || [ $JWB = 44 ] || [ $JWB = 45 ] || [ $JWB = 46 ] || [ $JWB = 47 ] || [ $JWB = 48 ] || [ $JWB = 49 ] || [ $JWB = 50 ]
+then 
+echo "Sedang Menambah Folowers Kamu" | lolcat
+sleep 3
+clear
+echo "Folowers Kamu Sedang di Proses."
+sleep 1
+clear
+echo "Folowers Kamu Sedang di Proses.."
+sleep 1
+clear
+echo "Folowers Kamu Sedang di Proses..."
+sleep 1
+clear
+echo "Folowers Kamu Sedang di Proses...."
+sleep 1
+clear
+echo "Folowers Kamu Sedang di Proses....."
+sleep 1
+clear
+echo "Folowers Kamu Sedang di Proses."
+sleep 1
+clear
+echo "Folowers Kamu Sedang di Proses.."
+sleep 1
+clear
+echo "Folowers Kamu Sedang di Proses..."
+sleep 1
+clear
+echo "Folowers Kamu Sedang di Proses...."
+sleep 1
+clear
+echo "Folowers Kamu Sedang di Proses....."
+sleep 1
+clear
+echo "Folowers Kamu Sedang di Proses."
+sleep 1
+clear
+echo "Folowers Kamu Sedang di Proses.."
+sleep 1
+clear
+echo "Folowers Kamu Sedang di Proses..."
+sleep 1
+clear
+echo "Folowers Kamu Sedang di Proses...."
+sleep 1
+clear
+echo "Folowers Kamu Sedang di Proses....."
+sleep 1
+clear
+echo "Folowers Kamu Sedang di Proses."
+sleep 1
+clear
+echo "Folowers Kamu Sedang di Proses.."
+sleep 1
+clear
+echo "Folowers Kamu Sedang di Proses..."
+sleep 1
+clear
+echo "Folowers Kamu Sedang di Proses...."
+sleep 1
+clear
+echo "Folowers Kamu Sedang di Proses....."
+sleep 1
+clear
+sleep 2
+toilet -f big -F gay Folowers IG
+echo "***********************NYX*********************************"|lolcat
+echo "User Name : $NM"|lolcat
+echo "Folowers Kamu Saat Ini : $FLW"|lolcat
+echo "Link IG : $LINK" | lolcat
+echo "***********************NYX*********************************"|lolcat
+sleep 1
+echo "Selamat Folowers Kamu Akan Kami Tambahkan" | lolcat
+echo "Mohon Tunggu" | lolcat
+echo "Folowers Akan Masuk Paling lambat 1-3 hari" | lolcat
+sleep 5
+clear
+echo "Bye Bye Datang lagi ya" | lolcat
+exit
+fi
+
+
+
+
+
